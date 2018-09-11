7590fdab0ad37629c64dc24fe46e04d1deb8938b
Sapulsic <carlos.barahona1995@gmail.com>
2018-09-11 17:09:31 -0500

    added images, styled page, and startup code to js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,23 @@
-// DOM
+// Global Variables
+//  ============================================
+// Arrays and Variables for initiating Data
+var randomNum = $('#randomNum');
+var winCount = $('#wins');
+var lossCount = $('#loss');
+var playerNum = $('#playerNum')
 
+var airElement = $('#air');
+var earthElement = $('#earth');
+var waterElement = $('#water');
+var fireElement = $('#fire');
 
-// ARRAYS
+// Counters
+winsCount = 0;
+lossCount = 0;
 
+// Functions
+//  ============================================
 
-// VARIABLES
+// Main Process
+//  ============================================
+
