2b6b9f7b0c22a739909f44e5ea97c019028f0147
Sapulsic <carlos.barahona1995@gmail.com>
2018-09-12 01:55:54 -0500

    made changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,10 @@
 // Global Variables
 //  ============================================
 // Arrays and Variables for initiating Data
-var randomNum = $('#randomNum');
 var winCount = $('#wins');
 var lossCount = $('#loss');
-var playerNum = $('#playerNum')
+var randomNumID = $('#randomNum');
+var playerNum = $('#playerNum');
 
 var airElement = $('#air');
 var earthElement = $('#earth');
@@ -14,10 +14,30 @@ var fireElement = $('#fire');
 // Counters
 winsCount = 0;
 lossCount = 0;
+playerNum = 0;
 
+// Random
+airElement = Math.floor(Math.random()* 12) + 1;
+earthElement = Math.floor(Math.random()* 12) + 1;
+waterElement = Math.floor(Math.random()* 12) + 1;
+fireElement = Math.floor(Math.random()* 12) + 1;
+
+randomNumID = randomNum;
+var randomNum = Math.floor(Math.random() * 120) + 19;
+
+console.log(airElement, earthElement, waterElement, fireElement, randomNum);
 // Functions
 //  ============================================
 
+function init() {
+    $('#randomNum').text(randomNumID);
+    $('#airElement').on('click');
+    $('#earthElement').on('click');
+    $('#waterElement').on('click');
+    $('#fireElement').on('click');
+}
+console.log(randomNumID)
+init()
 // Main Process
 //  ============================================
 
