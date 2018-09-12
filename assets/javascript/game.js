bf392463a215d314e957029a93ea0e0c11581afa
Sapulsic <carlos.barahona1995@gmail.com>
2018-09-12 04:00:03 -0500

    ade changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,44 @@
 // Global Variables
 //  ============================================
 // Arrays and Variables for initiating Data
-var winCount = $('#wins');
-var lossCount = $('#loss');
-var randomNumID = $('#randomNum');
-var playerNum = $('#playerNum');
+var $winCount = $('#wins');
+var $lossCount = $('#loss');
+var $randomNum = $('#randomNum');
+var $playerNum = $('#playerNum');
 
-var airElement = $('#air');
-var earthElement = $('#earth');
-var waterElement = $('#water');
-var fireElement = $('#fire');
+var $airElement = $('#air');
+var $earthElement = $('#earth');
+var $waterElement = $('#water');
+var $fireElement = $('#fire');
 
 // Counters
-winsCount = 0;
-lossCount = 0;
-playerNum = 0;
+$winCount = 0;
+$lossCount = 0;
+$playerNum = 0;
 
 // Random
-airElement = Math.floor(Math.random()* 12) + 1;
-earthElement = Math.floor(Math.random()* 12) + 1;
-waterElement = Math.floor(Math.random()* 12) + 1;
-fireElement = Math.floor(Math.random()* 12) + 1;
+$airElement = Math.floor(Math.random()* 12) + 1;
+$earthElement = Math.floor(Math.random()* 12) + 1;
+$waterElement = Math.floor(Math.random()* 12) + 1;
+$fireElement = Math.floor(Math.random()* 12) + 1;
 
-randomNumID = randomNum;
-var randomNum = Math.floor(Math.random() * 120) + 19;
+$randomNum = Math.floor(Math.random() * 120) + 19;
 
-console.log(airElement, earthElement, waterElement, fireElement, randomNum);
+console.log($airElement, $earthElement, $waterElement, $fireElement, $randomNum);
 // Functions
 //  ============================================
 
 function init() {
-    $('#randomNum').text(randomNumID);
-    $('#airElement').on('click');
-    $('#earthElement').on('click');
-    $('#waterElement').on('click');
-    $('#fireElement').on('click');
+    $('#airElement').on('click', function() {
+        alert($randomNum);
+    });
+
+
+
+
+    // Testing / Debugging
+    console.log($randomNum);
 }
-console.log(randomNumID)
-init()
 // Main Process
 //  ============================================
-
+init()
