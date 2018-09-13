f02207b82f95b500ee8f9c008336a388f70dae9e
Sapulsic <carlos.barahona1995@gmail.com>
2018-09-13 15:31:12 -0500

    made changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -22,25 +22,47 @@ var earthRandom = Math.floor(Math.random()* 12) + 1;
 var waterRandom = Math.floor(Math.random()* 12) + 1;
 var fireRandom = Math.floor(Math.random()* 12) + 1;
 
-console.log($fireElement)
+// console.log($fireElement)
 
 var random = Math.floor(Math.random() * 120) + 19;
 
 console.log(airRandom, earthRandom, waterRandom, fireRandom, random);
 // Functions
 //  ============================================
+$(document).ready(function (){
 
-function init() {
-    $('#airElement').on('click','img', function() {
-        playerNumCount += airRandom;
-        $('playerNum').html(playerNumCount);
-    });
+    
+    function init() {
+        debugger;
+        $('#air').on('click', function() {
+            playerNumCount += airRandom;
+            $('#playerNum').text(playerNumCount); 
+        });
+        
+        $('#earth').on('click', function() {
+            playerNumCount += earthRandom;
+            $('#playerNum').html(playerNumCount);
+        });
+        
+        $('#water').on('click', function() {
+            playerNumCount += waterRandom;
+            $('#playerNum').html(playerNumCount);
+        });
+        
+        $('#fire').on('click', function() {
+            playerNumCount += fireRandom;
+            $('#playerNum').html(playerNumCount);
+        });
+        
+        
+        
+        // Testing / Debugging
+        // console.log($);
+    }
+    // Main Process
+    //  ============================================
+    init()
 
 
 
-    // Testing / Debugging
-    // console.log($);
-}
-// Main Process
-//  ============================================
-init()
+});
